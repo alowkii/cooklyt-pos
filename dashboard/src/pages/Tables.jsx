f0546362872e99de415ae2fd52536e33a3dc1f1d
Aalok Pandit <paalok333@gmail.com>
2026-05-16 10:40:08 +0530

    feat: show QR code button for staff users on table cards

--- dashboard/src/pages/Tables.jsx
+++ dashboard/src/pages/Tables.jsx
@@ -322,7 +322,7 @@ export default function Tables() {
                 </span>
                 <TableStatusDot status={t.status} />
               </button>
-              {isAdmin && (
+              {canEdit && (
                 <button
                   onClick={() => handleQrClick(t)}
                   title="Show QR code"
